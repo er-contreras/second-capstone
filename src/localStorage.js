3f41450af66820290d4694c6964bec7f929a014b
Jocyline Magero <jmagero2017@gmail.com>
2021-08-12 15:40:30 -0500

    Create localStorage for data

---
+++ src/localStorage.js
@@ -0,0 +1,8 @@
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem('APIData'));
+
+const setLocalStorage = (data) => {
+  localStorage.setItem('APIData', JSON.stringify(data));
+};
+
+export { getLocalStorage, setLocalStorage };
