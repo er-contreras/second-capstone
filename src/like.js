f455c036fff272332bd8e5eceaa8378817e80d2c
Jocyline Magero <jmagero2017@gmail.com>
2021-08-12 15:40:04 -0500

    Add functions to display likes

--- src/like.js
+++ src/like.js
@@ -18,3 +18,30 @@ const postLike = async (id) => {
     const data = await response.json();
     return data;
   };
+
+  const displayLikes = (data) => {
+    const likeCounts = [...document.querySelectorAll('.likes')];
+    data.forEach((object) => {
+      const id = parseInt(object.item_id, 10);
+      likeCounts[id].textContent = object.likes;
+    });
+  };
+  
+  const getListLikes = () => {
+    getLikes().then((result) => {
+      displayLikes(result);
+    });
+  };
+  
+  const addLikes = (event) => {
+    const isLikeIcon = event.target.classList.contains('heart');
+    if (isLikeIcon) {
+      const { id } = event.target.dataset;
+      postLike(id).then((result) => {
+        getListLikes();
+        return result;
+      });
+    }
+  }
+
+  export { addLikes, displayLikes, getListLikes }
